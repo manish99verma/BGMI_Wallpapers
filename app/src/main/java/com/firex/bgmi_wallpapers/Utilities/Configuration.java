85a51bc92efe34655c99ac2422c9ac95880ed186
games <Manish_Admin>
2023-01-11 10:52:33 +0530

    Initial commit

---
+++ app/src/main/java/com/firex/bgmi_wallpapers/Utilities/Configuration.java
@@ -0,0 +1,8 @@
+package com.firex.bgmi_wallpapers.Utilities;
+
+import android.os.Environment;
+
+public class Configuration {
+    public static String appFolderPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pictures/BGMI's Wallpapers/";
+    public static String appFolderName = "BGMI's Wallpapers";
+}
